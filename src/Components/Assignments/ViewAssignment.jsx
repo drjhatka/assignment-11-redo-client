13ee0bc7c656edc3a30812ebbeccefa913885cbb
drjhatka <bishawjit12@gmail.com>
2024-07-15 01:09:05 -0400

    view assignment complete

--- src/Components/Assignments/ViewAssignment.jsx
+++ src/Components/Assignments/ViewAssignment.jsx
@@ -16,9 +16,27 @@ const ViewAssignment = () => {
                     assignmentData.filter(assignment => assignment._id == id).map((assignment, index) => {
 
                         console.log('---< ',assignmentData)
-                        return <div key={index}>
+                        return <div className='py-4 px-5 grid grid-cols-1 md:grid-cols-2' key={index}>
+                            <div>
                             <img src={"/"+assignment?.imageUrl} alt="Assignment Image" />
-                            <h1>{assignment?.title}</h1>
+
+                            </div>
+                                
+                            <div className='flex flex-col gap-5'>
+                                <h1 className='font-semibold shadow-lg text-xl text-center text-blue-600 py-4 border-b-2'>{assignment?.title}</h1>
+                                <div>
+                                    <h1 className='text-lg font-semibold text-red-700'>Assignment Description</h1>
+                                <p className='font-semibold py-3 px-4 text-justify shadow-lg border-2 rounded-md'>{assignment.description}</p>
+                                </div>
+                                <div className='flex border-b-2 py-3 justify-between'>
+                                    <span className='badge badge-info px-20 py-4 text-white font-semibold'>Difficulty : {assignment.difficulty.toUpperCase()}</span>
+                                    <span className='badge badge-secondary px-20 py-4 text-white font-semibold'>Marks : {assignment.marks.toUpperCase()}</span>
+                                </div>
+                                <div className='flex justify-center gap-10'>
+                                    <button className='btn btn-primary w-40 '>Update</button>
+                                    <button className='btn btn-primary w-40 '>Take Assignment</button>
+                                </div>
+                            </div>
                         </div>
 
                     })
